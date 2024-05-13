e6a814cd8ce79d83020afcb5c49b8779f50aef2e
melihabak <melihfurkanabak@gmail.com>
2024-05-13 22:27:45 +0300

    WifiTest ornegi degisik locatorlar kullanilarak yazildi

--- src/test/java/mfaudemy/AppiumBasics.java
+++ src/test/java/mfaudemy/AppiumBasics.java
@@ -5,6 +5,7 @@ import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
 import io.appium.java_client.service.local.AppiumDriverLocalService;
 import io.appium.java_client.service.local.AppiumServiceBuilder;
+import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
 import java.io.File;
@@ -17,8 +18,14 @@ public class AppiumBasics extends BaseTest{
 
         // Actual automation
         // XPath, id, accessibilityId, classname, androidUIAutomator
-        driver.findElement(AppiumBy.accessibilityId("Preference")).click();
+        // tagName = [@attribute='value'] is syntax for XPath
         // Set WiFi
+        driver.findElement(AppiumBy.accessibilityId("Preference")).click();
+        driver.findElement(By.xpath("//android.widget.TextView[@content-desc=\"3. Preference dependencies\"]")).click();
+        driver.findElement(By.id("android:id/checkbox")).click();
+        driver.findElement(By.xpath("(//android.widget.RelativeLayout)[2]")).click();
+        //driver.findElement();
+
 
 
     }
