1ee83a3b7c67e873165269d5a35d176b56e3cea6
melihabak <melihfurkanabak@gmail.com>
2024-05-14 18:05:51 +0300

    Uygulamanin istenilen sayfasindan baslanark istenilen testin kosuldugu bir ornek eklendi

--- src/test/java/mfaudemy/MisceallanousAppiumActions.java
+++ src/test/java/mfaudemy/MisceallanousAppiumActions.java
@@ -1,10 +1,14 @@
 package mfaudemy;
 
+import com.google.common.collect.ImmutableMap;
 import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.Activity;
 import io.appium.java_client.android.nativekey.AndroidKey;
 import io.appium.java_client.android.nativekey.KeyEvent;
 import org.openqa.selenium.By;
 import org.openqa.selenium.DeviceRotation;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.remote.RemoteWebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -13,8 +17,13 @@ public class MisceallanousAppiumActions extends BaseTest{
     @Test
     public void Miscellanous() throws InterruptedException {
 
-        driver.findElement(AppiumBy.accessibilityId("Preference")).click();
-        driver.findElement(By.xpath("//android.widget.TextView[@content-desc=\"3. Preference dependencies\"]")).click();
+        // App package and App Activity Sample
+        //Activity activity = new Activity("io.appium.android.apis","io.appium.android.apis.preference.PreferenceDependencies");
+        //driver.startActivity(activity); //deprecated
+        ((JavascriptExecutor) driver).executeScript("mobile: startActivity", ImmutableMap.of("intent","io.appium.android.apis/io.appium.android.apis.preference.PreferenceDependencies"));
+
+        //driver.findElement(AppiumBy.accessibilityId("Preference")).click();
+        //driver.findElement(By.xpath("//android.widget.TextView[@content-desc=\"3. Preference dependencies\"]")).click();
         driver.findElement(By.id("android:id/checkbox")).click();
         //Rotate screen
         DeviceRotation landScape = new DeviceRotation(0,0,90);
