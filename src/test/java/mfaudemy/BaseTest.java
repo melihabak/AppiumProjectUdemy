33151cfe7307975768789ef51cec3dc6f43f9cfe
melihabak <melihfurkanabak@gmail.com>
2024-05-14 11:28:27 +0300

    Scroll (Kaydirma) ornegi olusturuldu ve BaseTest kismina scrollForDesiredText ve scrollToEndAction metodlari eklendi

--- src/test/java/mfaudemy/BaseTest.java
+++ src/test/java/mfaudemy/BaseTest.java
@@ -1,6 +1,7 @@
 package mfaudemy;
 
 import com.google.common.collect.ImmutableMap;
+import io.appium.java_client.AppiumBy;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
 import io.appium.java_client.service.local.AppiumDriverLocalService;
@@ -46,6 +47,21 @@ public class BaseTest {
     {
         ((JavascriptExecutor) driver).executeScript("mobile: longClickGesture",
                 ImmutableMap.of("elementId",((RemoteWebElement) ele).getId(),"duration",2000));
+    }
+
+    public void scrollToEndAction(){
+        boolean canScrollMore;
+        do {
+            canScrollMore = (Boolean) ((JavascriptExecutor) driver).executeScript("mobile: scrollGesture", ImmutableMap.of(
+                    "left", 100, "top", 100, "width", 200, "height", 200,
+                    "direction", "down",
+                    "percent", 3.0
+            ));
+        }while (canScrollMore);
+    }
+
+    public void scrollForDesiredText(){
+        driver.findElement(AppiumBy.androidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"WebView\"));"));
     }
     @AfterClass
     public void tearDown(){
---
+++ src/test/java/mfaudemy/ScrollDemo.java
@@ -0,0 +1,47 @@
+package mfaudemy;
+
+import com.google.common.collect.ImmutableMap;
+import io.appium.java_client.AppiumBy;
+import org.openqa.selenium.JavascriptExecutor;
+import org.testng.annotations.Test;
+
+public class ScrollDemo extends BaseTest{
+    @Test
+    public void LongPressGesture() throws InterruptedException {
+
+        driver.findElement(AppiumBy.accessibilityId("Views")).click();
+
+        /*
+        No prior idea to scroll option
+        Java
+        boolean canScrollMore = (Boolean) ((JavascriptExecutor) driver).executeScript("mobile: scrollGesture", ImmutableMap.of(
+                "left", 100, "top", 100, "width", 200, "height", 200,
+                "direction", "down",
+                "percent", 1.0
+        ));
+        */
+
+        // Where to scroll is known prior
+        // driver.findElement(AppiumBy.androidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"WebView\"));"));
+        // Bu kod satiri BaseTest sinifinda metod (scrollForDesiredText()) olarak tanimlandi ve tekrar tekrar kod yaziminin onune gecildi
+
+        // scrollForDesiredText();
+
+        /*
+        boolean canScrollMore;
+        do {
+            canScrollMore = (Boolean) ((JavascriptExecutor) driver).executeScript("mobile: scrollGesture", ImmutableMap.of(
+                    "left", 100, "top", 100, "width", 200, "height", 200,
+                    "direction", "down",
+                    "percent", 3.0
+            ));
+        }while (canScrollMore);
+        Bu kod satiri BaseTest sinifinda metod (scrollToEndAction()) olarak tanimlandi ve tekrar tekrar kod yaziminin onune gecildi
+        */
+
+        scrollToEndAction();
+
+        Thread.sleep(2000);
+
+    }
+}
